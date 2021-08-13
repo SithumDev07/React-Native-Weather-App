3976ecdeb7e240d71d767aeab920d003ed541e84
SithumDev07 <sithum.dashantha@gmail.com>
2021-08-13 07:23:04 +0530

    Fetching coordinates from expo location

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import * as Location from 'expo-location'
+import { Alert, Text } from 'react-native';
 import Loading from './screens/Loading';
 
+
 export default function App() {
-  return (
-    <Loading />
-  );
+  const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+
+      try {
+        let {status} = await Location.requestForegroundPermissionsAsync();
+      // let { status } = await Permissions.askAsync(Permissions.LOCATION);
+      console.log(status);
+      if(status !== 'granted') {
+        console.log('Permission deneid');
+        return;
+      }
+
+      let {coords} = await Location.getCurrentPositionAsync();
+      setLoading(false);
+      console.log(coords);
+
+      //TODO - Send to API and Get Weather
+
+      } catch (error) {
+        Alert.alert("Can't find you", "So sad");
+        console.log(error);
+      }
+      
+    })();
+  }, [])
+
+
+  {return loading ? (<Text>Loading</Text>) : (<Loading />)}
 }
